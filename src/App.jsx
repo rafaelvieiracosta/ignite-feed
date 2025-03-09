bb5a86c5243281f68995048654790e49a1915399
rafaelvieiracosta <rvc.vieira@outlook.com>
2025-03-09 17:44:38 -0300

    feat: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { Header } from "./components/Header.jsx";
+import { Sidebar } from "./components/Sidebar.jsx";
+
+import "./global.css";
+
+import styles from "./App.module.css";
 
 export function App() {
   return (
     <>
       <Header />
-      <h1>Feed</h1>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>main</main>
+      </div>
     </>
   );
 }
