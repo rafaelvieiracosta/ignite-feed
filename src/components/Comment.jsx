1dfeda697ce0e459c337c07e6857ecd4b5e237e8
rafaelvieiracosta <rvc.vieira@outlook.com>
2025-03-12 21:25:30 -0300

    feat: post delete comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,11 @@ import styles from "./Comment.module.css";
 
 import { Avatar } from "./Avatar";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar
@@ -23,7 +27,7 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
