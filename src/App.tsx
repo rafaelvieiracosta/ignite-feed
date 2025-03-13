fdc5963f6907a181d44d7a9abef46c270df3bee8
rafaelvieiracosta <rvc.vieira@outlook.com>
2025-03-12 23:22:00 -0300

    fix: post type

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
+import { PostType } from "./components/Post";
+
 import "./global.css";
 
 import styles from "./App.module.css";
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -69,14 +71,7 @@ export function App() {
 
         <main>
           {posts.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            );
+            return <Post key={post.id} post={post} />;
           })}
         </main>
       </div>
