867861e78f3bfe6a6550f2eb1a688b0200f317e5
rafaelvieiracosta <rvc.vieira@outlook.com>
2025-03-12 23:09:29 -0300

    feat!: convert entire project to TypeScript

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
 
 import { format, formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale/pt-BR";
 
-import { Avatar } from "./Avatar.jsx";
-import { Comment } from "./Comment.jsx";
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
 
-export function Post({ author, publishedAt, content }) {
+interface Author {
+  name: string;
+  role: string;
+  avatarUrl: string;
+}
+
+interface Content {
+  type: string;
+  content: string;
+}
+
+interface PostProps {
+  author: Author;
+  publishedAt: Date;
+  content: Content[];
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
   const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
   const [newCommentText, setNewCommentText] = useState("");
 
@@ -23,23 +40,23 @@ export function Post({ author, publishedAt, content }) {
     addSuffix: true,
   });
 
-  function handleCreateNewComment() {
+  function handleCreateNewComment(event: FormEvent) {
     event.preventDefault();
 
     setComments([...comments, newCommentText]);
     setNewCommentText("");
   }
 
-  function handleNewCommentInvalid() {
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity("Esse campo é obrigatório");
   }
 
-  function handleCreateNewCommentChange() {
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
   }
 
-  function deleteComment(commentToDelete) {
+  function deleteComment(commentToDelete: string) {
     const commentsWithoutDeletedOne = comments.filter((comment) => {
       return comment !== commentToDelete;
     });
@@ -90,7 +107,7 @@ export function Post({ author, publishedAt, content }) {
           name="comment"
           placeholder="Deixe um comentário"
           value={newCommentText}
-          onChange={handleCreateNewCommentChange}
+          onChange={handleNewCommentChange}
           onInvalid={handleNewCommentInvalid}
           required
         />
