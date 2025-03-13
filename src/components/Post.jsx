e4bae300d268493d4e401017b577c52d43e7e5d8
rafaelvieiracosta <rvc.vieira@outlook.com>
2025-03-12 21:39:44 -0300

    feat: comment form validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,7 +30,12 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText("");
   }
 
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Esse campo é obrigatório");
+  }
+
   function handleCreateNewCommentChange() {
+    event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
   }
 
@@ -41,6 +46,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -84,10 +91,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleCreateNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
